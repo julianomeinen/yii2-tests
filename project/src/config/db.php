bd85b6eb36e59ef6aa9fcd319927e1d0a11d8318
Juliano Souza <julianomeinen.souza@gmail.com>
2022-12-07 12:15:44 -0700

    add private repo

--- project/src/config/db.php
+++ project/src/config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => getenv('DB_DSN'),
-    'username' => getenv('DB_USERNAME'),
-    'password' => getenv('DB_PASSWORD'),
+    'dsn' => getenv('DB_DSN') || 'pgsql:host=postgres;dbname=postgres',
+    'username' => getenv('DB_USERNAME') || 'postgres',
+    'password' => getenv('DB_PASSWORD') || 'postgres',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
--- project/src/config/test_db.php
+++ project/src/config/test_db.php
@@ -1,6 +1,6 @@
 <?php
 $db = require __DIR__ . '/db.php';
 // test database! Important not to run tests on production or development databases
-$db['dsn'] = getenv('TEST_DB_DSN');
+$db['dsn'] = getenv('TEST_DB_DSN') || 'pgsql:host=postgres;dbname=postgres';
 
 return $db;
